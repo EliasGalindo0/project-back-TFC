cfdb5de072c796458b549c9c8e6e87e694e6fb71
EliasGalindo0 <eliasgalindo@gmail.com>
2022-08-18 18:24:57 -0300

    feat: create users migrations and model

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,55 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default User;
