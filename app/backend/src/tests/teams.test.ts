875d4a26ca392d0c81cfb6339fc31aeb44c6f97e
EliasGalindo0 <eliasgalindo@gmail.com>
2022-08-27 23:53:25 -0300

    feat: test teams

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,32 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teams', () => {
+  
+  beforeEach(async () => {
+    sinon.restore();
+  });
+  
+  it('should return status 200', async () => {
+
+      const chaiHttpResponse = await chai.request(app)
+        .post('/teams');
+      
+      expect(chaiHttpResponse.status).to.equal(200);
+    })
+
+  it('should return status 200 when get a Team By Id', async () => {
+
+      const chaiHttpResponse = await chai.request(app)
+        .post('/teams/1');
+      
+      expect(chaiHttpResponse.status).to.equal(200);
+    })
+  })
