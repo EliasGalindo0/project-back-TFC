f9fff368d1d1db2a27599035542e9a16ef3f0930
EliasGalindo0 <eliasgalindo@gmail.com>
2022-08-29 22:13:21 -0300

    fix: auhtentication error catch

--- app/backend/src/middleWares/authentication.ts
+++ app/backend/src/middleWares/authentication.ts
@@ -1,14 +1,17 @@
-import 'dotenv/config';
 import * as jwt from 'jsonwebtoken';
 import ValidateError from './ValidateError';
+import 'dotenv/config';
 
 const secret = process.env.JWT_SECRET || '123456';
 
 export const auth = (token: string) => {
   if (!token) throw new ValidateError(401, 'Token not found');
-  const data = jwt.verify(token, secret) as jwt.JwtPayload;
-  if (!data) throw new ValidateError(401, 'Token must be a valid token');
-  return data;
+  try {
+    const data = jwt.verify(token, secret) as jwt.JwtPayload;
+    return data;
+  } catch (error) {
+    throw new ValidateError(401, 'Token must be a valid token');
+  }
 };
 
 export const verifyToken = (password: string) => jwt.verify(password, secret);
